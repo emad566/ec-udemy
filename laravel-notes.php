e73f5a12661e20a3bc855587f7f4ca5643b4fb3b
Emadeldeen Abdallah <emad_566@hotmail.comm>
2021-01-14 03:25:28 +0200

    Basic First Commit

--- laravel-notes.php
+++ laravel-notes.php
@@ -1,3 +1,15 @@
 =======================================
 # 000: Laravel Framework: 8.22.1
 =======================================
+you need to install  nodejs
+
+=======================================
+# 001: Section 1: Introduction
+=======================================
+https://github.com/easylearningbd/laravelecommerce
+
+=======================================
+# 002: Section 2: Laravel 8 A-Z Basic Fandamentals with Complete Dynamic Web Project
+=======================================
+to get node version: 
+node -v
