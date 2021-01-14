657a309d26a950ccc299b51420a66558bb394b05
Emadeldeen Abdallah <emad_566@hotmail.comm>
2021-01-14 04:48:53 +0200

    002:Auth Jetstream Install

--- laravel-notes.php
+++ laravel-notes.php
@@ -2,6 +2,8 @@
 # 000: Laravel Framework: 8.22.1
 =======================================
 you need to install  nodejs
+to get node version: 
+node -v
 
 =======================================
 # 001: Section 1: Introduction
@@ -11,5 +13,36 @@ https://github.com/easylearningbd/laravelecommerce
 =======================================
 # 002: Section 2: Laravel 8 A-Z Basic Fandamentals with Complete Dynamic Web Project
 =======================================
-to get node version: 
-node -v
+composer create-project laravel/laravel basic
+
+
+//-- 13. What is Route
+-----------------------------
+Six Types of Route Method
+1- Get
+2- Post
+3- Put
+4- Delete
+5- Patch
+6- options
+
+//-- 17. Middleware
+-----------------------------
+To create middleware add it in the Kernal.php
+
+//-- from  20. Laravel 8 Authentication install
+-----------------------------
+Laravel_8 auth different from laravel_7 auth
+
+
+** laravel_7 auth installions
+composer require laravel/ui:^2.4
+php artisan ui vue --auth
+
+** laravel_8 auth installions
+composer require laravel/jetstream
+php artisan jetstream:install livewire
+npm install && npm run dev
+
+php artisan migrate
+php artisan serve
